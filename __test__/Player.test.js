2d1fcc7dd21f2edca8a2823df20cef5bd4405b08
Lesly <leslyr562@gmail.com>
2021-10-21 15:11:17 -0500

    player health objects

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -1,4 +1,3 @@
-const { expect, test } = require('@jest/globals');
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion')
 jest.mock('../lib/Potion');
@@ -34,3 +33,31 @@ test('get inventory from player or return false', () => {
 
     expect(player.getInventory()).toEqual(false)
 });
+
+test('get players health value', () => {
+    const player = new Player('Dave');
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
+  });
--- __test__/Potion.test.js
+++ __test__/Potion.test.js
@@ -6,4 +6,4 @@ test('creates a random potion object', () => {
     expect(potion.name).toEqual(expect.any(String));
     expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
-  });
+  }); 
